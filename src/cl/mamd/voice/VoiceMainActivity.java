fc97b71282e8f72458bc4137c10b9662d9c223ff
Manuel Alejandro Moscoso Dominguez <manuel.moscoso.d@gmail.com>
2014-03-06 00:40:55 -0300

    Actualizando archivos de prueba Conexion WIFI

--- src/cl/mamd/voice/VoiceMainActivity.java
+++ src/cl/mamd/voice/VoiceMainActivity.java
@@ -1,8 +1,14 @@
 package cl.mamd.voice;
 
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.view.Menu;
+import android.widget.Toast;
 
 public class VoiceMainActivity extends Activity {
 
@@ -10,6 +16,22 @@ public class VoiceMainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_voice_main);
+        
+        ConnectivityManager connManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo mWifi = connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+
+        if (mWifi.isConnected()) {
+            // Do whatever
+        	Toast.makeText(this,getResources().getString(R.string.wifi_connection_state) 
+        			+ "OK", Toast.LENGTH_LONG).show();
+        }
+        else {
+        	Toast.makeText(this,getResources().getString(R.string.wifi_connection_state) 
+        			+ "OFF", Toast.LENGTH_LONG).show();
+        	
+        	startActivity(new Intent(Settings.ACTION_WIFI_SETTINGS));
+        }
+        
     }
 
 
